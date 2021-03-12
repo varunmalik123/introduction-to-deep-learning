d58083390888c8b28152b7982ece1339a72c1ab2
Varun Malik <varunmalik123@gmail.com>
2021-03-11 22:14:05 -0500

    Add files via upload

---
+++ neural-network-scratch/relu.py
@@ -0,0 +1,25 @@
+from .base_layer import BaseLayer
+import numpy as np
+
+class ReLU(BaseLayer):
+	def __init__(self):
+		self.cache = None
+
+	def forward(self, 
+		input_x: np.ndarray
+	):
+		## TODO: Implement RELU activation function forward pass
+		input_x[input_x < 0] = 0
+		output = input_x
+		# Store the input in cache, required for backward pass
+		self.cache = input_x.copy()
+		return output
+
+	def backward(self, dout):
+		# Load the input from the cache
+		x_temp = self.cache
+		# Calculate gradient for RELU 
+		x_temp[x_temp < 0] = 0 
+		x_temp[x_temp > 0] = 1
+		dx = dout * x_temp
+		return dx
---
+++ neural-network-scratch/softmax.py
@@ -0,0 +1,45 @@
+from .base_layer import BaseLayer
+import numpy as np
+
+class Softmax(BaseLayer):
+	'''Implement the softmax layer
+	Output of the softmax passes to the Cross entropy loss function
+	The gradient for the softmax is calculated in the loss_func backward pass
+	Thus the backward function here should be an empty pass'''
+	def __init__(self):
+		pass
+
+	def forward(self,
+		input_x: np.ndarray
+	):	
+		N, C = input_x.shape # Remember input_x is not the input samples, but the output from the last layer just before softmax
+		# Thus here, N=Number of samples, C = number of classes
+		# TODO: Implement the softmax layer forward pass
+		# For each of the 10 outputs, the score is given by e_i/sum(e_j) where i is the output from ith class and j sums
+		# over the outputs for all the classes. e here is the exponential function
+
+		# scores matrix must be of the dimension NxC, where C is the number of classes
+		
+
+		scores = input_x - np.max(input_x, axis=-1, keepdims=True) # avoid numeric instability
+
+		# Calculate softmax outputs e_i/sum(e_j)
+	
+
+		softmax_temp = np.zeros([N,C])
+		
+		for  i in range(N):
+			T_iter = np.exp(input_x[i,:] - input_x[i,:].max(keepdims = True))
+			to_return = T_iter / T_iter.sum()
+			# print(to_return)
+			softmax_temp[i,:] = to_return
+
+		softmax_matrix = softmax_temp
+
+		assert scores.shape==input_x.shape, "Scores must be NxC"
+
+		return softmax_matrix
+
+	def backward(self, dout):
+		# Nothing to do here, pass. The gradient are calculated in the cross entropy loss backward function itself
+		return dout
